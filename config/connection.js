f42f149d1eb551eabf8961a6c9aff0580e93a30f
Grace Rhee <gracemrh@gmail.com>
2018-06-22 21:11:35 -0500

    setup database for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,18 @@ var connection = mysql.createConnection({
   database: 'burgers_db'
 })
 
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 6603,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'})
+}
+
 connection.connect(err => {
   if (err) {
     console.error('error connecing ' + err.stack);
