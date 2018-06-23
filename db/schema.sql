4d6fd532bb99fe1bccb86575251b66cb8a67cd82
kyhakw <kyhakw@hotmail.com>
2018-06-22 22:25:17 -0500

    modify schema.js

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,8 @@ use burgers_db;
 
 Create TABLE burgers (
   id int not null auto_increment,
-  burger_name varchar(50) not null,
+  burger_name varchar(255) not null,
   devoured boolean not NULL,
+  created_at TIMESTAMP not null default CURRENT_TIMESTAMP,
   primary key(id)
 )
