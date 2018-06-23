87dd5f50c6775e21c91c0bd1577f49d390573057
kyhakw <kyhakw@hotmail.com>
2018-06-22 21:29:59 -0500

    add connection.js

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 6603,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'})
+}
+
+connection.connect(err => {
+  if (err) {
+    console.error('error connecing ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
