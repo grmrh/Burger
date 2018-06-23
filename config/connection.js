79d7ebdfd238b4a27b14816cc18fa59576290206
Grace Rhee <gracemrh@gmail.com>
2018-06-22 21:19:33 -0500

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,5 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: '127.0.0.1',
-  port: 6603,
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
-})
-
 var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
